47731d7df8f14bfa372c9137cdb5d01db5527c21
Brycew30 <brycew30@gmail.com>
2020-09-28 13:39:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map (sourceArray, callBack) {
+  let arr = []
+  for (let i=0; i < sourceArray.length; i++) {
+    let method = sourceArray[i]
+    arr.push(callBack(method))
+  }
+  return arr
+}
+
+function reduce(sourceArray, callBack, startingPoint) {
+  let arr = (!!startingPoint) ? startingPoint : sourceArray[0]
+  let i = (!!startingPoint) ? 0 : 1
+  for ( ; i < sourceArray.length; i++) {
+    arr = callBack(sourceArray[i], arr)
+  }
+  return arr
+}
